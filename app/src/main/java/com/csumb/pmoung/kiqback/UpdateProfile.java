8d39ec4fdf295071915decea9c2e3627c0052d48
BRX0102 <brono@csumb.edu>
2016-10-27 16:35:20 -0700

    UpdateProfile set up for database sending code.

--- app/src/main/java/com/csumb/pmoung/kiqback/UpdateProfile.java
+++ app/src/main/java/com/csumb/pmoung/kiqback/UpdateProfile.java
@@ -1,5 +1,6 @@
 package com.csumb.pmoung.kiqback;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -22,7 +23,11 @@ public class UpdateProfile extends AppCompatActivity implements View.OnClickList
     @Override
     public void onClick(View v) {
         if(v.getId() == R.id.updateNextButton){
+//            Send the data to the database
 
+//            Move to the main activity
+            Intent toMain = new Intent(this, MainActivity.class);
+            startActivity(toMain);
         }
     }
 }
