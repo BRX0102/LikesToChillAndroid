4c1235b329664412a4f7e1388003a50f6ea38dd0
BRX0102 <brono@csumb.edu>
2016-10-31 23:03:48 -0700

    Added two tests to User

---
+++ app/src/test/java/com/csumb/pmoung/kiqback/UserTest.java
@@ -0,0 +1,48 @@
+package com.csumb.pmoung.kiqback;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by BRX01 on 10/31/2016.
+ */
+public class UserTest {
+    private User userTest;
+    @Before
+    public void create(){
+        userTest = new User(1, "Sean", "O'Fallon", "93955", "sofallon@csumb.edu", "M", "2016-10-27", "About Sean");
+    }
+
+    @Test
+    public void getUserId() throws Exception {
+        Boolean idIs1 = false;
+        try{
+            if(userTest.getUserId() == 1)
+            {
+                idIs1=true;
+            }
+        }catch(Exception e){
+            idIs1=false;
+        }
+
+        assertTrue(idIs1);
+    }
+
+    @Test
+    public void setUserId() throws Exception {
+        Boolean idIs100 = false;
+        try{
+            userTest.setUserId(100);
+            if(userTest.getUserId() == 100)
+            {
+                idIs100=true;
+            }
+        }catch(Exception e){
+            idIs100=false;
+        }
+
+        assertTrue(idIs100);
+    }
+}
