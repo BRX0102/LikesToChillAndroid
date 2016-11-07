22d98643af7b6505ffd273a8b43b818210837a1b
Luv2C0de <pmoung@csumb.edu>
2016-11-06 20:42:16 -0800

    moved fb login to activity

---
+++ app/src/androidTest/java/com/csumb/pmoung/kiqback/LoginTest.java
@@ -0,0 +1,88 @@
+package com.csumb.pmoung.kiqback;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Context;
+import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.action.CoordinatesProvider;
+import android.support.test.espresso.action.GeneralClickAction;
+import android.support.test.espresso.action.Press;
+import android.support.test.espresso.action.Tap;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+
+import com.facebook.FacebookActivity;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static java.lang.Thread.sleep;
+
+/**
+ * Created by Peter on 11/5/2016.
+ */
+@RunWith(AndroidJUnit4.class)
+public class LoginTest {
+
+    @Rule
+    public ActivityTestRule<Login> mLoginActivityTestRule = new ActivityTestRule<Login>(Login.class);
+
+    @Test
+    public void mainScreen_loads() {
+        onView(withId(R.id.text_details)).check(matches(withText("Welcome!")));
+    }
+
+    @Test
+    public void clickFaceBookLoginButton_opensLogin() throws Exception {
+        onView(withId(R.id.login_button))
+                .perform(click());
+        try {
+            sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        clickXY(542, 886);
+        typeText("luv2splooge@gmail.com");
+        clickXY(555, 1014);
+        typeText("4658516Pm1");
+        clickXY(533, 668);
+        onView(withId(R.id.text_details))
+                .check(matches(withText("Welcome Ritty Phon")));
+    }
+
+
+    public static ViewAction clickXY(final int x, final int y){
+        return new GeneralClickAction(
+                Tap.SINGLE,
+                new CoordinatesProvider() {
+                    @Override
+                    public float[] calculateCoordinates(View view) {
+
+                        final int[] screenPos = new int[2];
+                        view.getLocationOnScreen(screenPos);
+
+                        final float screenX = screenPos[0] + x;
+                        final float screenY = screenPos[1] + y;
+                        float[] coordinates = {screenX, screenY};
+
+                        return coordinates;
+                    }
+                },
+                Press.FINGER);
+    }
+
+}
